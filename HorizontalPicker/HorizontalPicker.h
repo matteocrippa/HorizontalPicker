98e93770779edc502f15331476ff04f58c11145a
Andrew Crookston <andrew@caoos.com>
2016-02-26 12:31:06 -0800

    Initial Commit

---
+++ HorizontalPicker/HorizontalPicker.h
@@ -0,0 +1,19 @@
+//
+//  HorizontalPicker.h
+//  HorizontalPicker
+//
+//  Created by Andrew C on 2/26/16.
+//  Copyright © 2016 Andrew Crookston. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for HorizontalPicker.
+FOUNDATION_EXPORT double HorizontalPickerVersionNumber;
+
+//! Project version string for HorizontalPicker.
+FOUNDATION_EXPORT const unsigned char HorizontalPickerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HorizontalPicker/PublicHeader.h>
+
+
